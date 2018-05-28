aaa70ca2d3e298ec8ea36002e6785d51f0a93b6f
Dovydas Venckus <dovydas.venck@gmail.com>
2018-05-29 01:02:20 +0300

    Add correct encoding to use while reading sodra file

--- src/main/java/com/dovydasvenckus/sodra/parser/SodraParser.java
+++ src/main/java/com/dovydasvenckus/sodra/parser/SodraParser.java
@@ -11,7 +11,7 @@ public class SodraParser {
     public static void main(String[] args) throws IOException {
         Arrays.stream(args).forEach(System.out::println);
 
-        Files.lines(Paths.get(args[0]), Charset.forName("ISO-8859-1"))
+        Files.lines(Paths.get(args[0]), Charset.forName("ISO-8859-13"))
                 .limit(10)
                 .forEach(System.out::println);
     }
